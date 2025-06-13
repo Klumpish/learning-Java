c9ba4e9c203654dbe9cfdebf50591b79ece0348d
Henrik <Henrikmattsson89@gmail.com>
2025-06-13 23:27:29 +0200

    more algorithems and problem solving

--- TheLargestPrimeFactor/src/Main.java
+++ TheLargestPrimeFactor/src/Main.java
@@ -1,2 +1,36 @@
 public class Main {
+    public static void main(String[] args) {
+
+
+//        System.out.println(getLargestPrime(45));
+//        System.out.println(getLargestPrime(1));
+//        System.out.println(getLargestPrime(7));
+//        System.out.println(getLargestPrime(21));
+        System.out.println(printStuff(5));
+    }
+
+    public static int getLargestPrime(int number) {
+        if (number < 2) {
+            return -1;
+        }
+        int largest = 0;
+        for (int i = 2; i <= number; i++) {
+            while (number % i == 0) {
+                largest = i;
+                number = number / i;
+            }
+        }
+
+        return largest;
+    }
+
+    public static String printStuff(int number) {
+        if (number <= 0) return "Invalid Number - Pick a number above 1.";
+
+        for (int i = 1; i <= number; i++) {
+            System.out.println("Counting: " + i);
+
+        }
+        return "";
+    }
 }
