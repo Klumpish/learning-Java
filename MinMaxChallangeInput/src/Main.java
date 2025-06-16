ba27454c4e9650b067fe88f354e2367ed2fef347
Henrik <Henrikmattsson89@gmail.com>
2025-06-16 15:46:00 +0200

    worked on input from user, and some problems from udemey with input and overloaded methods

---
+++ MinMaxChallangeInput/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean isCorrect = true;
+        int count = 1;
+        double min = 100;
+        double max = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (isCorrect) {
+            System.out.println("Input a number #" + count + ": ");
+            String input = scanner.nextLine();
+            try {
+                double number = Double.parseDouble(input);
+                min = Math.min(min, number);
+                max = Math.max(max, number);
+                count++;
+                if (count >= 6) {
+                    isCorrect = false;
+                }
+
+            } catch (Exception e) {
+                System.out.println(
+                  "Invalid input @" + count + ", the program will exit");
+                System.out.println(
+                  "this was the min / max inputs: " + min + "/" + max);
+                isCorrect = false;
+            }
+
+        }
+        System.out.println("this was the min // max inputs: " + min + "/" + max);
+    }
+}
