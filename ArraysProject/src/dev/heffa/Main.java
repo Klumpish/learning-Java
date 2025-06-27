7eeaf4a81b6a79866bda45fdf282c503f9927705
Henrik <Henrikmattsson89@gmail.com>
2025-06-27 13:59:25 +0200

    learning about packages and Arrays

---
+++ ArraysProject/src/dev/heffa/Main.java
@@ -0,0 +1,14 @@
+package dev.heffa;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] myIntArray = new int[10];
+        myIntArray[5] = 50;
+
+        double[] myDoubleArray = new double[10];
+        myDoubleArray[2] = 3.5;
+
+        System.out.println(myDoubleArray[2]);
+    }
+}
