12dae3273c6aba16dd7865f335132530cba814d8
Henrik <Henrikmattsson89@gmail.com>
2025-07-04 17:53:30 +0200

    Stupid lists

--- ArrayLists/src/dev/heffa/Main.java
+++ ArrayLists/src/dev/heffa/Main.java
@@ -1,8 +1,50 @@
 package dev.heffa;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem(String name, String type, int count) {
+    public GroceryItem(String name) {
+        this(name, "DAIRY", 1);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %s in %s", count, name.toUpperCase(), type);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
 
-        
+//            this works. but only cuz we init with Object[]
+//        Object[] groceryArray = new Object[3];
+//        groceryArray[0] = new GroceryItem("milk");
+//        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+//        groceryArray[2] = "5 oranges";
+        GroceryItem[] groceryArray = new GroceryItem[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
+        System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
+
+//        how to specify what kind of list it is
+//        <> is called the diamond operator
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("milk"));
+        groceryList.add(new GroceryItem("oranges", "PRODUCE", 5));
+//        with a index we place the item there and push everything else down
+        groceryList.add(0, new GroceryItem("apples", "PRODUCE", 6));
+//        with set we replace whats on that index, removing it.
+        groceryList.set(1, new GroceryItem("apples", "PRODUCE", 6));
+//        remove just removes whats on that index.
+        groceryList.remove(2);
+        System.out.println(groceryList);
+
     }
 }
