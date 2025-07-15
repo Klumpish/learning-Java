5984b6823f9394530bd93098f7327f603410e39d
Henrik <Henrikmattsson89@gmail.com>
2025-07-15 18:13:36 +0200

    .

--- AutoBoxing_ArrayList_challange/src/dev/heffa/teacher/Main.java
+++ AutoBoxing_ArrayList_challange/src/dev/heffa/teacher/Main.java
@@ -1,11 +1,12 @@
 package dev.heffa.teacher;
 
+
 import java.util.ArrayList;
 
-record Customer(String name, ArrayList<Double> transactions) {
+record Customer2(String name, ArrayList<Double> transactions) {
 
 
-    public Customer(String name, double initialDeposit) {
+    public Customer2(String name, double initialDeposit) {
         this(name.toUpperCase(), new ArrayList<Double>(500));
         transactions.add(initialDeposit);
     }
@@ -14,20 +15,37 @@ record Customer(String name, ArrayList<Double> transactions) {
 public class Main {
     public static void main(String[] args) {
 
-        Customer bob = new Customer("Bob S", 1000.0);
+        Customer2 bob = new Customer2("Bob S", 1000.0);
         System.out.println(bob);
+
+        Bank2 bank2 = new Bank2("Chase");
+        bank2.addNewCustomer("Jane A", 500.0);
+        System.out.println(bank2);
+
+        bank2.addTransaction("Jane A", -10.25);
+        bank2.addTransaction("Jane A", -75.01);
+        bank2.printStatement("Jane A");
+        bank2.addNewCustomer("Heffa L", 30);
+        bank2.addTransaction("Heffa L", 300.0);
+        bank2.printStatement("heffa L");
     }
 }
 
-class Bank {
+class Bank2 {
     private String name;
-    private ArrayList<Customer> customers = new ArrayList<>(5000);
+    private ArrayList<Customer2> customers = new ArrayList<>(5000);
 
-    public Bank(String name) {
+    public Bank2(String name) {
         this.name = name;
     }
 
-    private Customer getCustomer(String customerName) {
+    @Override
+    public String toString() {
+        return "Bank2{" + "name='" + name + '\'' + ", customers=" + customers +
+          '}';
+    }
+
+    private Customer2 getCustomer(String customerName) {
 
         for (var customer : customers) {
             if (customer.name().equalsIgnoreCase(customerName)) {
@@ -40,9 +58,31 @@ class Bank {
 
     public void addNewCustomer(String customerName, double initialDeposit) {
         if (getCustomer(customerName) == null) {
-            Customer customer = new Customer(customerName, initialDeposit);
+            Customer2 customer = new Customer2(customerName, initialDeposit);
             customers.add(customer);
             System.out.println("New Customer added: " + customer);
         }
+    }
+
+    public void addTransaction(String name, double transactionAmount) {
+        Customer2 customer2 = getCustomer(name);
+        if (customer2 != null) {
+            customer2.transactions().add(transactionAmount);
+        }
+    }
+
+    public void printStatement(String customerName) {
+        Customer2 customer2 = getCustomer(customerName);
+        if (customer2 == null) {
+            return;
+        }
+        System.out.println("-".repeat(30));
+        System.out.println("Customer Name: " + customer2.name());
+        System.out.println("Transactions:");
+        for (double d : customer2.transactions()) { //unboxing
+            System.out.printf("$%10.2f (%s)%n", d, d < 0 ? "debit" : "credit");
+        }
+
+
     }
 }
