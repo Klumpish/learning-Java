c9713205a063afe0ff049bb36bd7c146cc49db84
Henrik <Henrikmattsson89@gmail.com>
2025-07-20 11:54:34 +0200

    learning about enum's

---
+++ EnumProject/src/dev/heffa/Main.java
@@ -0,0 +1,49 @@
+package dev.heffa;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DayOfTheWeek weekDay = DayOfTheWeek.TUES;
+        System.out.println(weekDay);
+
+        for (int i = 0; i < 10; i++) {
+            weekDay = getRandomDay();
+
+//            System.out.printf("Name is %s, Ordinal value = %d%n",
+//              weekDay.name(), weekDay.ordinal());
+//
+//            if (weekDay == DayOfTheWeek.FRI) {
+//                System.out.println("Found a Friday");
+//            }
+            switchDayOfWeek(weekDay);
+        }
+        System.out.println("-".repeat(20));
+        for (Toppings topping : Toppings.values()) {
+            System.out.println(topping.name() + " : " + topping.getPrice());
+        }
+    }
+
+    public static void switchDayOfWeek(DayOfTheWeek weekDay) {
+        int weekDayInteger = weekDay.ordinal() + 1;
+
+        switch (weekDay) {
+            case WED ->
+              System.out.println("Wednesday is Day " + weekDayInteger);
+            case SAT -> System.out.println("Saturday is Day " + weekDayInteger);
+            default -> System.out.println(weekDay.name().charAt(0) +
+              weekDay.name().substring(1).toLowerCase() + "day is Day " +
+              weekDayInteger);
+        }
+    }
+
+    public static DayOfTheWeek getRandomDay() {
+//        the 7 tells us we wont get anything higher than a 6
+        int randomInteger = new Random().nextInt(7);
+        var allDays = DayOfTheWeek.values();
+
+        return allDays[randomInteger];
+    }
+
+}
